6f6eb0b8a09a5a44b1955d49cc1fa7bc022525e5
luisfcv97 <luisfcv97@gmail.com>
2020-03-10 19:11:40 -0500

    show a image

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './config/vuetify'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import vuetify from './config/vuetify';
+import  Home from './views/Home';
+import Pets from './views/Pets';
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/pets", component: Pets }
+]
+
+const router = new VueRouter({routes});
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Pets.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container grid-list-md fluid>
+      <v-row>
+          <v-col>
+              <v-card>
+                  <v-img src="https://goo.gl/6CQNDo"></v-img>
+                    <v-card-title>
+                        <div>
+                            <h3>
+                                Looking for a dog?
+                            </h3>
+                        </div>
+                    </v-card-title>   
+              </v-card>
+          </v-col>
+      </v-row>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
